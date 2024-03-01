944ceaab0e10b6bbc8b8e1f63ac8790b231a4c41
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-03-01 15:20:13 +0300

    created project file via spring initializer

---
+++ src/main/java/erat/feyza/FeyzaApplication.java
@@ -0,0 +1,13 @@
+package erat.feyza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeyzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeyzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erat/feyza/FeyzaApplicationTests.java
@@ -0,0 +1,13 @@
+package erat.feyza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeyzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
