4aa88663cd0c076eafc5e8790699d93737351c63
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-03-01 18:10:12 +0300

    created first entity

---
+++ src/main/java/erat/feyza/models/User.java
@@ -0,0 +1,30 @@
+package erat.feyza.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
